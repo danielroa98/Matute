7e7c67a2ca40f9ffe894dba2da249f2e98b27452
Starfleet-Command <31284116+Starfleet-Command@users.noreply.github.com>
2017-09-07 19:45:38 -0500

    1/2 de la tarea del reloj

--- reloj.java
+++ reloj.java
@@ -5,9 +5,9 @@ public class reloj
   public static void main(String[] args)
   {
     Scanner lectura = new Scanner(System.in);
-    int hora, min, sec, horasec, minsec;
+    int hora, min, sec, horasec, minsec,doce,distancia1,distancia2,suma;
 
-    System.out.println("Inserta la hora en doce horas");
+    System.out.println("Inserta la hora en formato de 24 horas");
     hora = lectura.nextInt();
 
     System.out.println("Inserta los minutos");
@@ -18,7 +18,22 @@ public class reloj
 
     horasec = hora * 360;
     minsec = min * 60;
+    doce= 43200;
+    suma = horasec + minsec + sec;
 
-    horasec
+    if (suma<=doce) {
+    distancia1= doce-suma;
+    System.out.printf("El tiempo transcurrido desde la ultima rotacion de doce horas es %d segundos. %n",distancia1);
+
+
+    }
+
+    else if (suma>doce) {
+      distancia2= suma-doce;
+      System.out.printf("El tiempo transcurrido desde la ultima rotacion de doce horas es %d segundos. %n",distancia2);
+
+
+    }
+
   }
 }
